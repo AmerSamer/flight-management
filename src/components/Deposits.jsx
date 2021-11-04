bd4e29ef01953034e29bc593157e23b4ce83d316
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2021-11-04 04:47:08 +0200

    flights management

--- src/components/Deposits.jsx
+++ src/components/Deposits.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Spinner from './Spinner';
+// import Spinner from './Spinner';
 function Deposits({ cart }) {
     return (
         <div className="ui container">
